ea74418968500db43cf17ad0bc7a0bedc5940988
Andrei Kaleshka <ka8725@gmail.com>
2022-10-16 22:49:41 +0200

    fix railtie loading

--- lib/actual_db_schema.rb
+++ lib/actual_db_schema.rb
@@ -4,5 +4,5 @@ require_relative "actual_db_schema/version"
 
 # The main module definition
 module ActualDbSchema
-  require "actual_db_schema/railtie" if defined?(Rails)
+  require "railtie" if defined?(Rails)
 end
