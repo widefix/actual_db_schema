28fb69869e9da3ab76f7ff6a1b07630982f0e2b2
Andrei Kaleshka <ka8725@gmail.com>
2024-01-03 14:36:29 +0100

    refactor

--- lib/actual_db_schema/commands/list.rb
+++ lib/actual_db_schema/commands/list.rb
@@ -1,25 +1,32 @@
+# frozen_string_literal: true
+
 module ActualDbSchema
   module Commands
+    # Shows the list of phantom migrations
     class List
-      def call
-        unless ActualDbSchema.config.fetch(:enabled, true)
-          raise "ActualDbSchema is disabled. Set ActualDbSchema.config[:enabled] = true to enable it."
-        end
+      private
 
-        context = ActiveRecord::Base.connection.migration_context
-        context.extend(ActualDbSchema::Patches::MigrationContext)
+      def call_impl
+        preambule
+        table
+      end
 
+      def indexed_phantom_migrations
+        @indexed_phantom_migrations ||= context.migrations.index_by { |m| m.version.to_s }
+      end
+
+      def preambule
         puts "\nPhantom migrations\n\n"
         puts "Below is a list of irrelevant migrations executed in unmerged branches."
         puts "To bring your database schema up to date, the migrations marked as \"up\" should be rolled back."
         puts "\ndatabase: #{ActiveRecord::Base.connection_db_config.database}\n\n"
         puts %(#{"Status".center(8)}  #{"Migration ID".ljust(14)}  Migration File)
         puts "-" * 50
+      end
 
-        phantom_migrations = context.migrations.index_by { |m| m.version.to_s }
-
+      def table
         context.migrations_status.each do |status, version|
-          migration = phantom_migrations[version]
+          migration = indexed_phantom_migrations[version]
           next unless migration
 
           puts %(#{status.center(8)}  #{version.to_s.ljust(14)}  #{migration.filename.gsub("#{Rails.root}/", "")})
--- lib/actual_db_schema/commands/rollback.rb
+++ lib/actual_db_schema/commands/rollback.rb
@@ -1,25 +1,21 @@
+# frozen_string_literal: true
+
 module ActualDbSchema
   module Commands
+    # Rolls back all phantom migrations
     class Rollback
-      def call
-        unless ActualDbSchema.config.fetch(:enabled, true)
-          raise "ActualDbSchema is disabled. Set ActualDbSchema.config[:enabled] = true to enable it."
-        end
+      private
 
-        if ActiveRecord::Migration.current_version >= 6
-          ActiveRecord::Tasks::DatabaseTasks.raise_for_multi_db(command: "db:rollback_branches")
-        end
-
-        context = ActiveRecord::Base.connection.migration_context
-        context.extend(ActualDbSchema::Patches::MigrationContext)
+      def call_impl
         context.rollback_branches
-        if ActualDbSchema.failed.any?
-          puts ""
-          puts "[ActualDbSchema] Irreversible migrations were found from other branches. Roll them back or fix manually:"
-          puts ""
-          puts ActualDbSchema.failed.map { |migration| "- #{migration.filename}" }.join("\n")
-          puts ""
-        end
+
+        return if ActualDbSchema.failed.empty?
+
+        puts ""
+        puts "[ActualDbSchema] Irreversible migrations were found from other branches. Roll them back or fix manually:"
+        puts ""
+        puts ActualDbSchema.failed.map { |migration| "- #{migration.filename}" }.join("\n")
+        puts ""
       end
     end
   end
