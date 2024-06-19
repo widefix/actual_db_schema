8db6da08be8156e8754f11769cc3bc006b02d31b
Vladislav Sokov <vladislav.sokov.92@gmail.com>
2024-06-19 19:59:35 +0300

    fixup! fixup! fixup! Add tests

--- test/dummy_app/db/secondary_schema.rb
+++ test/dummy_app/db/secondary_schema.rb
@@ -10,5 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2013_09_06_111515) do
 end
