a45030d13652ce5d0c2fae64be68bb03104aa364
Vladislav Sokov <vladislav.sokov.92@gmail.com>
2024-07-04 16:04:48 +0300

    fixup! fixup! Add Migration class

--- app/controllers/actual_db_schema/migrations_controller.rb
+++ app/controllers/actual_db_schema/migrations_controller.rb
@@ -10,41 +10,18 @@ module ActualDbSchema
     end
 
     def rollback
-      rollback_migration(params[:id], params[:database])
+      ActualDbSchema::Migration.rollback(params[:id], params[:database])
       redirect_to migrations_path
     end
 
     private
 
     helper_method def migrations
-      ActualDbSchema::Migration.instance.all
+      ActualDbSchema::Migration.all
     end
 
     helper_method def migration
       ActualDbSchema::Migration.find(params[:id], params[:database])
-    end
-
-    def rollback_migration(version, database)
-      ActualDbSchema.for_each_db_connection do
-        next unless ActualDbSchema.db_config[:database] == database
-
-        context = ActualDbSchema.prepare_context
-        if context.migrations.detect { |m| m.version.to_s == version }
-          context.run(:down, version.to_i)
-          break
-        end
-      end
-    end
-
-    def build_migration_struct(status, migration)
-      MigrationStruct.new(
-        status: status,
-        version: migration.version.to_s,
-        name: migration.name,
-        branch: ActualDbSchema.branch_for(ActualDbSchema.metadata, migration.version),
-        database: ActualDbSchema.db_config[:database],
-        filename: migration.filename
-      )
     end
   end
 end
