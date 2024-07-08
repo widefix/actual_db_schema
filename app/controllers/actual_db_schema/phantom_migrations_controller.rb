6fe74f28ae8358eae280beb8efb28584fa5033f8
Vladislav Sokov <vladislav.sokov.92@gmail.com>
2024-07-08 19:44:32 +0300

    Rename migrations to phantom migrations

---
+++ app/controllers/actual_db_schema/phantom_migrations_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module ActualDbSchema
+  # Controller to display the list of phantom migrations for each database connection.
+  class PhantomMigrationsController < ActionController::Base
+    def index; end
+
+    def show
+      render :not_found, status: 404 unless phantom_migration
+    end
+
+    def rollback
+      ActualDbSchema::Migration.rollback(params[:id], params[:database])
+      redirect_to phantom_migrations_path
+    end
+
+    private
+
+    helper_method def phantom_migrations
+      @phantom_migrations ||= ActualDbSchema::Migration.all
+    end
+
+    helper_method def phantom_migration
+      @phantom_migration ||= ActualDbSchema::Migration.find(params[:id], params[:database])
+    end
+  end
+end
