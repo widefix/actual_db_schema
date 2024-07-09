7acc076f2311b05fa0c9d6bfb48dadfe981bc214
Vladislav Sokov <vladislav.sokov.92@gmail.com>
2024-07-09 16:52:51 +0300

    Add migrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 ActualDbSchema::Engine.routes.draw do
+  resources :migrations, only: %i[index]
   resources :phantom_migrations, only: %i[index show] do
     member do
       post :rollback
