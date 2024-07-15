b6e51f5954468f849eb0cd4d2c5f6615fd2e6a9b
Vladislav Sokov <vladislav.sokov.92@gmail.com>
2024-07-15 20:14:06 +0300

    Change the sorting to DESC

--- test/dummy_app/db/secondary_schema.rb
+++ test/dummy_app/db/secondary_schema.rb
@@ -10,6 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2013_09_06_111513) do
+ActiveRecord::Schema.define(version: 2013_09_06_111515) do
 
 end
