549e4381c336a0b7f44afb789d3ce2f01cee3ccc
Mane Darbinyan <122614161+m-darbinyan@users.noreply.github.com>
2025-01-27 14:57:25 +0400

    Add require logger (#123)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "logger"
 require "rails/all"
 require "actual_db_schema"
 require "minitest/autorun"
